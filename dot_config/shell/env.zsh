c728d022d405d2a551e84d068b45ecfab6c73630
lohbrandt <lohbrandt@gmail.com>
2025-07-19 18:43:49 +0200

    feat: update OLLAMA environment variables and paths in env.zsh; fix nodejs and python versions in dot_tool-versions; add Ruby to PATH in dot_zshrc

--- dot_config/shell/env.zsh
+++ dot_config/shell/env.zsh
@@ -151,5 +151,37 @@ export DOWNLOADS_DIR="$HOME/Downloads"
 # Set default documents directory
 export DOCUMENTS_DIR="$HOME/Documents"
 
-# OLLAMA Models directory
-export OLLAMA_MODELS="/Volumes/llm_models"
+# OLLAMA enviroment variables
+# OLLAMA_HOST: IP-Adresse und Port, auf denen der Ollama-Server lauscht (Standard: 127.0.0.1:11434).
+# OLLAMA_DEBUG: Aktiviert erweiterte Debug-Ausgaben (1 für aktiviert).
+# OLLAMA_KEEP_ALIVE: Gibt an, wie lange Modelle im Speicher gehalten werden (z.B. 5m für 5 Minuten).
+# OLLAMA_MODELS bzw. OLLAMA_MODEL_DIR: Pfad zum Verzeichnis, in dem Modelle abgelegt werden.
+# OLLAMA_MAX_LOADED_MODELS: Maximale Anzahl gleichzeitig geladener Modelle (Standard: 1).
+# OLLAMA_MAX_QUEUE: Länge der Anfrage-Warteschlange (Standard: 512).
+# OLLAMA_NUM_PARALLEL: Maximale Anzahl paralleler Anfragen (Standard: 1).
+# OLLAMA_NOPRUNE: Unterdrückt das Bereinigen von Modelldateien beim Start (1 = kein Bereinigen).
+# OLLAMA_ORIGINS: Komma-separierte Liste erlaubter Origins für CORS-Anfragen.
+# OLLAMA_TMPDIR: Pfad für temporäre Dateien.
+# OLLAMA_FLASH_ATTENTION: Beschleunigt die Token-Generierung, insbesondere auf Apple Silicon oder mit NVIDIA-GPUs (1 = aktiviert).
+# OLLAMA_LLM_LIBRARY: Erzwingt die Nutzung einer bestimmten LLM-Bibliothek (z.B. cuda_v11, cpu, rocm_v6).
+# OLLAMA_MAX_VRAM: Maximale VRAM-Nutzung in Bytes.
+# OLLAMA_NOHISTORY: Deaktiviert die History-Funktion (1 = aktiviert).
+# OLLAMA_SCHED_SPREAD: Modellverteilung auf alle Video-Adapter (1 = aktiviert).
+# OLLAMA_MULTIUSER_CACHE: Optimiert den Prompt-Cache für Multiuser-Umgebungen.
+# OLLAMA_CONTEXT_LENGTH: Setzt die Kontextfenstergröße (z.B. 4096, Standard: 2048).
+# OLLAMA_NEW_ENGINE: Aktiviert eine experimentelle neue Ausführungs-Engine.
+# OLLAMA_GPU_OVERHEAD: Reserviert einen bestimmten VRAM-Bereich pro GPU in Bytes.
+# OLLAMA_NUM_GPUS: Legt die Anzahl verwendeter GPUs fest.
+# OLLAMA_CACHE_DIR: Pfad für den Cache-Speicherort.
+export OLLAMA_MODELS="/Volumes/docs/llm_models"
+export OLLAMA_NUM_PARALLEL=2
+export OLLAMA_TMPDIR="/Volumes/temp"
+export OLLAMA_NEW_ENGINE=1
+export OLLAMA_FLASH_ATTENTION=1
+export OLLAMA_HOST=0.0.0.0:11434
+export OLLAMA_KEEP_ALIVE=10m
+
+
+# Ruby compilers directory
+export LDFLAGS="-L/opt/homebrew/opt/ruby/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/ruby/include"
